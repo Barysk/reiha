5806458a183e155fb0985e4d1da1c6f6327f68d0
Boris <boris.komarov@proton.me>
2025-05-07 23:12:58 +0200

    added virtual_screen
    used macroquad-canvas library, probably going to rewrite it by myself in future

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 use macroquad::prelude::*;
+use macroquad_canvas::Canvas2D;
 
 const DEFAULT_FONT: &[u8; 7835672] = include_bytes!("../fonts/ipaexm.ttf");
+
+const VIRTUAL_SCREEN: Vec2 = vec2(1600f32, 1200f32);
 
 #[macroquad::main("レイハ")]
 async fn main() {
     
-    let fnt: Font = load_ttf_font_from_bytes(DEFAULT_FONT).unwrap();
+    let font: Font = load_ttf_font_from_bytes(DEFAULT_FONT).unwrap();
     println!("{}x{}",screen_width(),screen_height());
     println!("{}",get_time());
 
+    let virtual_screen = Canvas2D::new(VIRTUAL_SCREEN.x, VIRTUAL_SCREEN.y);
+
     loop {
         clear_background(BLACK);
+        {
+            set_camera(&virtual_screen.camera);
+            clear_background(GRAY);
+            
 
-        draw_text_center("Test", Some(&fnt));
-
+            draw_text_center("Test", Some(&font));
+            set_default_camera();
+        }
+        virtual_screen.draw();
         draw_fps();
         next_frame().await
     }
@@ -27,13 +38,17 @@ fn draw_text_center (text: &str, font: Option<&Font>) {
 
     // font size adatable
     let font_size = find_max_font_size(text, font, font_scale);
+    
+    //let font_size = 96u16;
 
     // set position to center of the screen
-    let mut position = vec2(screen_width()/2f32, screen_height()/2f32);
+    //let mut position = vec2(screen_width()/2f32, screen_height()/2f32);
+    let mut position = vec2(VIRTUAL_SCREEN.x/2f32, VIRTUAL_SCREEN.y/2f32);
     
     // make true position
-    position.x -= measure_text(text, font, font_size, font_scale).width / 2f32;
-    position.y += measure_text(text, font, font_size, font_scale).height / 2f32;
+    let text_dimentions = measure_text(text, font, font_size, font_scale);
+    position.x -= text_dimentions.width / 2f32;
+    position.y += text_dimentions.height / 2f32;
 
     draw_text_ex(
         text,
@@ -51,8 +66,8 @@ fn draw_text_center (text: &str, font: Option<&Font>) {
 }
 
 fn find_max_font_size(text: &str, font: Option<&Font>, font_scale: f32) -> u16 {
-    let screen_w = screen_width();
-    let screen_h = screen_height();
+    let screen_w = VIRTUAL_SCREEN.y;
+    let screen_h = VIRTUAL_SCREEN.x;
     let target_width = screen_w * 0.9;
     let target_height = screen_h * 0.9;
 
